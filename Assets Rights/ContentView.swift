becb0b36dca9882e40b976c403b1d75e4e46f035
Gerson Carlos <gerson.mtg@gmail.com>
2019-06-30 08:17:17 -0300

    Initial Commit

---
+++ Assets Rights/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Assets Rights
+//
+//  Created by Gerson Carlos on 30/06/19.
+//  Copyright © 2019 Gerson Carlos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
---
+++ Assets RightsTests/Assets_RightsTests.swift
@@ -0,0 +1,34 @@
+//
+//  Assets_RightsTests.swift
+//  Assets RightsTests
+//
+//  Created by Gerson Carlos on 30/06/19.
+//  Copyright © 2019 Gerson Carlos. All rights reserved.
+//
+
+import XCTest
+@testable import Assets_Rights
+
+class Assets_RightsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
